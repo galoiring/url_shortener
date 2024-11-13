ab51b0e1a949f6accd41888bca4f0cdd85b9fe25
gal <gal.oiring@gmail.com>
2024-11-13 17:13:18 +0200

    add url validatior

--- url_shortener/shortener/views.py
+++ url_shortener/shortener/views.py
@@ -3,6 +3,20 @@ from django.http import JsonResponse
 from django.conf import settings
 import json
 from .models import URL
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+
+def validate_url(url):
+    validator = URLValidator()
+    try:
+        # Add http:// if no protocol specified
+        if not url.startswith(('http://', 'https://')):
+            url = 'http://' + url
+        validator(url)
+        return url
+    except ValidationError:
+        return None
 
 
 def home(request):
@@ -15,9 +29,14 @@ def create_short_url(request):
             data = json.loads(request.body)
             original_url = data.get('url')
 
+            # Validate URL
+            valid_url = validate_url(original_url)
+            if not valid_url:
+                return JsonResponse({'error': 'Invalid URL format'}, status=400)
+
             # Check if URL already exists
             existing_url = URL.objects.filter(
-                original_url=original_url).first()
+                original_url=valid_url).first()
             if existing_url:
                 short_url = request.build_absolute_uri(
                     f'/s/{existing_url.short_code}')
@@ -26,13 +45,15 @@ def create_short_url(request):
             # Create new shortened URL
             short_code = URL.generate_short_code()
             URL.objects.create(
-                original_url=original_url,
+                original_url=valid_url,
                 short_code=short_code
             )
 
             short_url = request.build_absolute_uri(f'/s/{short_code}')
             return JsonResponse({'short_url': short_url})
 
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
         except Exception as e:
             return JsonResponse({'error': str(e)}, status=400)
 
